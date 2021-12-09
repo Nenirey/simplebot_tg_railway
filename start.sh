74230f6eeade9bd438912d8697b9263e681bc553
Reinier Romero Mir <nenirey@gmail.com>
2021-12-09 15:00:22 -0500

    fix

--- start.sh
+++ start.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+BOTPATH="${ADDR/@/"%40"}"
 BOTZIPDB="${ADDR/@/"%40"}.zip"
-echo "$BOTZIPDB"
-python3 ./restore.py
-python3 -m simplebot init "$ADDR" "$PASSWORD"
-if [ -f "$BOTZIPDB" ]; then
-    echo "Bot restaurado..."
+BOTDB="$HOME/.simplebot/accounts/$BOTPATH/bot.db"
+echo "BOTPATH = $BOTPATH"
+echo "BOTZIPDB = $BOTZIPDB"
+echo "BOTDB = $BOTDB"
+if [ -f "$BOTDB" ]; then
+   echo "Bot ya inicializado!"
+   python3 -m simplebot init "$ADDR" "$PASSWORD"
 else
-    python3 -m simplebot --account "$ADDR" plugin --add ./telebridge.py
-    python3 -m simplebot --account "$ADDR" admin --add "$ADMIN"
+   echo "Restaurando..."
+   python3 ./restore.py
+   python3 -m simplebot init "$ADDR" "$PASSWORD"
+   if [ -f "$BOTZIPDB" ]; then
+      echo "Bot restaurado!"
+      rm "$BOTZIPDB"
+   else
+      echo "No existen restauras, configurando..."
+      python3 -m simplebot --account "$ADDR" plugin --add ./telebridge.py
+      python3 -m simplebot --account "$ADDR" admin --add "$ADMIN"
+   fi
 fi
 python3 -m simplebot --account "$ADDR" --show-ffi serve
