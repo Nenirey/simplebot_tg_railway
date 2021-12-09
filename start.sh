9c049a7c789a82b133f5f6d71a76436d0c89a58d
Reinier Romero Mir <nenirey@gmail.com>
2021-12-09 14:01:37 -0500

    update start script

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+BOTZIPDB="${ADDR/@/"%40"}.zip"
+echo "$BOTZIPDB"
+python3 ./restore.py
 python3 -m simplebot init "$ADDR" "$PASSWORD"
-python3 -m simplebot --account "$ADDR" plugin --add ./telebridge.py
-python3 -m simplebot --account "$ADDR" admin --add "$ADMIN"
-python3 -m simplebot --account "$ADDR" serve
+if [ -f "$BOTZIPDB" ]; then
+    echo "Bot restaurado..."
+else
+    python3 -m simplebot --account "$ADDR" plugin --add ./telebridge.py
+    python3 -m simplebot --account "$ADDR" admin --add "$ADMIN"
+fi
+python3 -m simplebot --account "$ADDR" --show-ffi serve
